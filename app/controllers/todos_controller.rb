44ccdeddfbaaf42ddd0e5547aa0555cc1c647847
EditB <beditto@yahoo.com>
2018-03-09 09:56:50 +1100

    Created an Edit/Update feature

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,6 +15,20 @@ class TodosController < ApplicationController
 
   def show
     @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was successfully updated"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end    
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+
   end
 
   private
